53538ff046b7f763f1a293e84d82c1682194b042
obfuscurity <jason@dixongroup.net>
2012-05-11 08:22:28 -0400

    normalize our local iterators

--- public/j/tasseo.js
+++ public/j/tasseo.js
@@ -14,97 +14,97 @@ function constructUrl(index, period) {
 
 // build our graph objects
 function constructGraphs() {
-  for (var j=0; j<metrics.length; j++) {
-    constructUrl(j, period);
-    aliases[j] = metrics[j].alias || metrics[j].target;
-    datum[j] = [{ x:0, y:0 }];
-    graphs[j] = new Rickshaw.Graph({
-      element: document.querySelector('.graph' + j),
+  for (var i=0; i<metrics.length; i++) {
+    constructUrl(i, period);
+    aliases[i] = metrics[i].alias || metrics[i].target;
+    datum[i] = [{ x:0, y:0 }];
+    graphs[i] = new Rickshaw.Graph({
+      element: document.querySelector('.graph' + i),
       width: 350,
       height: 100,
       interpolation: 'step-after',
       series: [{
-        name: aliases[j],
+        name: aliases[i],
         color: '#afdab1',
-        data: datum[j]
+        data: datum[i]
       }]
     });
-    graphs[j].render();
+    graphs[i].render();
   }
 }
 
 // refresh the graph
 function refreshData(immediately) {
 
-  for (var k=0; k<graphs.length; k++) {
-    getData(function(n, values) {
-      for (var x=0; x<values.length; x++) {
-        datum[n][x] = values[x];
+  for (var i=0; i<graphs.length; i++) {
+    getData(function(j, values) {
+      for (var k=0; k<values.length; k++) {
+        datum[j][k] = values[k];
       }
 
       // check our thresholds and update color
-      var lastValue = datum[n][datum[n].length - 1].y;
-      var warning = metrics[n].warning;
-      var critical = metrics[n].critical;
+      var lastValue = datum[j][datum[j].length - 1].y;
+      var warning = metrics[j].warning;
+      var critical = metrics[j].critical;
       if (critical > warning) {
         if (lastValue > critical) {
-          graphs[n].series[0].color = '#d59295';
+          graphs[j].series[0].color = '#d59295';
         } else if (lastValue > warning) {
-          graphs[n].series[0].color = '#f5cb56';
+          graphs[j].series[0].color = '#f5cb56';
         } else {
-          graphs[n].series[0].color = '#afdab1';
+          graphs[j].series[0].color = '#afdab1';
         }
       } else {
         if (lastValue < critical) {
-          graphs[n].series[0].color = '#d59295';
+          graphs[j].series[0].color = '#d59295';
         } else if (lastValue < warning) {
-          graphs[n].series[0].color = '#f5cb56';
+          graphs[j].series[0].color = '#f5cb56';
         } else {
-          graphs[n].series[0].color = '#afdab1';
+          graphs[j].series[0].color = '#afdab1';
         }
       }
       // we want to render immediately, i.e.
       // as soon as ajax completes
       // used for time period / pause view
       if (immediately) {
-        updateGraphs(n);
+        updateGraphs(j);
       }
-    }, k);
+    }, i);
   }
   // we can wait until all data is gathered, i.e.
   // the live refresh should happen synchronously
   if (!immediately) {
-    for (var q=0; q<graphs.length; q++) {
-      updateGraphs(q);
+    for (var i=0; i<graphs.length; i++) {
+      updateGraphs(i);
     }
   }
 }
 
 // pull data from graphite
-function getData(cb, n) {
+function getData(cb, i) {
   var myDatum = [];
-  if (metrics[n].target !== false) {
+  if (metrics[i].target !== false) {
     $.ajax({
       dataType: 'jsonp',
       jsonp: 'jsonp',
       error: function(xhr, textStatus, errorThrown) { console.log(errorThrown); },
-      url: urls[n]
+      url: urls[i]
     }).done(function(d) {
       if (d.length > 0) {
         myDatum[0] = {
           x: d[0].datapoints[0][1],
-          y: d[0].datapoints[0][0] || graphs[n].lastKnownValue || 0
+          y: d[0].datapoints[0][0] || graphs[i].lastKnownValue || 0
         };
-        for (var m=1; m<d[0].datapoints.length; m++) {
-          myDatum[m] = {
-            x: d[0].datapoints[m][1],
-            y: d[0].datapoints[m][0] || graphs[n].lastKnownValue
+        for (var j=1; j<d[0].datapoints.length; j++) {
+          myDatum[j] = {
+            x: d[0].datapoints[j][1],
+            y: d[0].datapoints[j][0] || graphs[i].lastKnownValue
           };
-          if (typeof d[0].datapoints[m][0] === "number") {
-            graphs[n].lastKnownValue = d[0].datapoints[m][0];
+          if (typeof d[0].datapoints[j][0] === "number") {
+            graphs[i].lastKnownValue = d[0].datapoints[j][0];
           }
         }
-        cb(n, myDatum);
+        cb(i, myDatum);
       }
     });
   }
@@ -112,27 +112,27 @@ function getData(cb, n) {
 
 // perform the actual graph object and
 // overlay name and number updates
-function updateGraphs(m) {
+function updateGraphs(i) {
   // update our graph
-  graphs[m].update();
-  if (metrics[m].target === false) {
+  graphs[i].update();
+  if (metrics[i].target === false) {
     //continue;
-  } else if (datum[m][datum[m].length - 1] !== undefined) {
-    var lastValue = datum[m][datum[m].length - 1].y;
+  } else if (datum[i][datum[i].length - 1] !== undefined) {
+    var lastValue = datum[i][datum[i].length - 1].y;
     var lastValueDisplay;
     if ((typeof lastValue == 'number') && lastValue < 2.0) {
       lastValueDisplay = Math.round(lastValue*1000)/1000;
     } else {
       lastValueDisplay = parseInt(lastValue)
     }
-    $('.overlay-name' + m).text(aliases[m]);
-    $('.overlay-number' + m).text(lastValueDisplay);
-    if (metrics[m].unit) {
-      $('.overlay-number' + m).append('<span class="unit">' + metrics[m].unit + '</span>');
+    $('.overlay-name' + i).text(aliases[i]);
+    $('.overlay-number' + i).text(lastValueDisplay);
+    if (metrics[i].unit) {
+      $('.overlay-number' + i).append('<span class="unit">' + metrics[i].unit + '</span>');
     }
   } else {
-    $('.overlay-name' + m).text(aliases[m])
-    $('.overlay-number' + m).html('<span class="error">NF</span>');
+    $('.overlay-name' + i).text(aliases[i])
+    $('.overlay-number' + i).html('<span class="error">NF</span>');
   }
 }
 
@@ -153,12 +153,12 @@ if (myTheme === "dark") { enableNightMode(); }
 
 // initial load screen
 refreshData();
-for (var g=0; g<graphs.length; g++) {
-  if (metrics[g].target === false) {
+for (var i=0; i<graphs.length; i++) {
+  if (metrics[i].target === false) {
   } else if (myTheme === "dark") {
-    $('.overlay-number' + g).html('<img src="/i/spin-night.gif" />');
+    $('.overlay-number' + i).html('<img src="/i/spin-night.gif" />');
   } else {
-    $('.overlay-number' + g).html('<img src="/i/spin.gif" />');
+    $('.overlay-number' + i).html('<img src="/i/spin.gif" />');
   }
 }
 
