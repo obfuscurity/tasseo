9c6b5dbb5552ecb4e4dc0cbae7cb211301403953
Mathias Meyer <meyer@paperplanes.de>
2012-07-27 23:15:17 +0200

    Extract Librato Metrics code.
    Basis for supporting multiple transports.

--- public/j/tasseo.js
+++ public/j/tasseo.js
@@ -1,196 +1,219 @@
 
-var graphs = {};      // rickshaw objects
-var datum = {};       // metric data
-var aliases = {};     // alias strings
-var realMetrics = {}; // non-false targets
+var LibratoMetrics = {
+  url: 'https://metrics-api.librato.com/v1/metrics',
+  graphs: {},      // rickshaw objects
+  datum: {},       // metric data
+  aliases: {},     // alias strings
+  realMetrics: {}, // non-false targets
+  // minutes of data in the live feed
+  period: (typeof period == 'undefined') ? 20 : period,
 
-// minutes of data in the live feed
-var period = (typeof period == 'undefined') ? 20 : period;
-
-// gather our non-false targets
-function gatherRealMetrics() {
-  for (var metric in metrics) {
-    if (metrics[metric].target === false) {
-    } else {
-      var name = metrics[metric].target + '-' + metrics[metric].source
-      realMetrics[name] = metrics[metric];
-      realMetrics[name]['selector'] = metrics[metric].target.replace(/[\.:]/g, '-');
-    }
-  }
-}
-
-// build our graph objects
-function constructGraphs() {
-  for (var metric in realMetrics) {
-    var name = realMetrics[metric].target + '-' + realMetrics[metric].source
-    var alias = realMetrics[metric].alias || realMetrics[metric].target;
-    aliases[name] = alias;
-    datum[name] = [{ x:0, y:0 }];
-    graphs[name] = new Rickshaw.Graph({
-      element: document.querySelector('.graph' + realMetrics[metric].selector),
-      width: 348,
-      height: 100,
-      interpolation: 'step-after',
-      series: [{
-        name: aliases[name],
-        color: '#afdab1',
-        data: datum[name]
-      }]
-    });
-    graphs[name].render();
-  }
-}
-
-// construct url
-function constructUrl(metric) {
-  var source = realMetrics[metric].source;
-  var target = realMetrics[metric].target;
-  var now = new Date();
-  var offset = now.getTimezoneOffset();
-  now = parseInt((now.getTime() + offset * 60) / 1000);
-  var start = now - (period * 60);
-  var end = now;
-  return url + '/' + encodeURI(target) + '?source=' + source + '&resolution=1&start_time=' + start + '&end_time=' + end;
-}
-
-// refresh the graph
-function refreshData(immediately) {
-  for (var metric in realMetrics) {
-    getData(metric, function(metric, values) {
-      for (var i = 0; i < values.length; i++) {
-        if (typeof values[i] !== "undefined") {
-          datum[metric][i] = values[i];
-        }
-      }
-      // check our thresholds and update color
-      var lastValue = datum[metric][datum[metric].length - 1].y;
-      var warning = realMetrics[metric].warning;
-      var critical = realMetrics[metric].critical;
-      if (critical > warning) {
-        if (lastValue >= critical) {
-          graphs[metric].series[0].color = '#d59295';
-        } else if (lastValue >= warning) {
-          graphs[metric].series[0].color = '#f5cb56';
-        } else {
-          graphs[metric].series[0].color = '#afdab1';
-        }
+  // gather our non-false targets
+  gatherRealMetrics: function() {
+    for (var metric in this.metrics) {
+      if (this.metrics[metric].target === false) {
       } else {
-        if (lastValue <= critical) {
-          graphs[metric].series[0].color = '#d59295';
-        } else if (lastValue <= warning) {
-          graphs[metric].series[0].color = '#f5cb56';
-        } else {
-          graphs[metric].series[0].color = '#afdab1';
-        }
+        var name = this.metrics[metric].target + '-' + this.metrics[metric].source
+        this.realMetrics[name] = this.metrics[metric];
+        this.realMetrics[name]['selector'] = this.metrics[metric].target.replace(/[\.:]/g, '-');
       }
-      // we want to render immediately, i.e.
-      // as soon as ajax completes
-      // used for time period / pause view
-      if (immediately) updateGraph(metric);
-    });
-    values = null;
-  }
-
-  // we can wait until all data is gathered, i.e.
-  // the live refresh should happen synchronously
-  if (!immediately) {
-    for (var graph in graphs) {
-      updateGraph(graph);
     }
-  }
-}
+  },
 
-function displayTransform(value, period, transform) {
-  if (transform !== undefined && transform !== null && transform.length > 0) {
-    var p = period, x = value;
-    return eval(transform);
-  } else {
-    return value;
-  }
-}
-// retrieve the data from Graphite
-function getData(metric, cb) {
-  var myDatum = [];
-  $.ajax({
-    beforeSend: function(xhr) {
-      if (auth.length > 0) {
-        var bytes = Crypto.charenc.Binary.stringToBytes(auth);
-        var base64 = Crypto.util.bytesToBase64(bytes);
-        xhr.setRequestHeader("Authorization", "Basic " + base64);
+  // build our graph objects
+  constructGraphs: function() {
+    for (var metric in this.realMetrics) {
+      var name = this.realMetrics[metric].target + '-' + this.realMetrics[metric].source
+      var alias = this.realMetrics[metric].alias || this.realMetrics[metric].target;
+      this.aliases[name] = alias;
+      this.datum[name] = [{ x:0, y:0 }];
+      this.graphs[name] = new Rickshaw.Graph({
+        element: document.querySelector('.graph' + this.realMetrics[metric].selector),
+        width: 348,
+        height: 100,
+        interpolation: 'step-after',
+        series: [{
+          name: this.aliases[name],
+          color: '#afdab1',
+          data: this.datum[name]
+        }]
+      });
+      this.graphs[name].render();
+    }
+  },
+
+  // construct url
+  constructUrl: function(metric) {
+    var source = this.realMetrics[metric].source;
+    var target = this.realMetrics[metric].target;
+    var now = new Date();
+    var offset = now.getTimezoneOffset();
+    now = parseInt((now.getTime() + offset * 60) / 1000);
+    var start = now - (this.period * 60);
+    var end = now;
+    return this.url + '/' + encodeURI(target) + '?source=' + source + '&resolution=1&start_time=' + start + '&end_time=' + end;
+  },
+
+  // refresh the graph
+  refreshData: function(immediately) {
+    for (var metric in this.realMetrics) {
+      this.getData(metric, function(metric, values) {
+        for (var i = 0; i < values.length; i++) {
+          if (typeof values[i] !== "undefined") {
+            this.datum[metric][i] = values[i];
+          }
+        }
+        // check our thresholds and update color
+        var lastValue = this.datum[metric][this.datum[metric].length - 1].y;
+        var warning = this.realMetrics[metric].warning;
+        var critical = this.realMetrics[metric].critical;
+        if (critical > warning) {
+          if (lastValue >= critical) {
+            this.graphs[metric].series[0].color = '#d59295';
+          } else if (lastValue >= warning) {
+            this.graphs[metric].series[0].color = '#f5cb56';
+          } else {
+            this.graphs[metric].series[0].color = '#afdab1';
+          }
+        } else {
+          if (lastValue <= critical) {
+            this.graphs[metric].series[0].color = '#d59295';
+          } else if (lastValue <= warning) {
+            this.graphs[metric].series[0].color = '#f5cb56';
+          } else {
+            this.graphs[metric].series[0].color = '#afdab1';
+          }
+        }
+        // we want to render immediately, i.e.
+        // as soon as ajax completes
+        // used for time period / pause view
+        if (immediately) this.updateGraph(metric);
+      });
+      values = null;
+    }
+
+    // we can wait until all data is gathered, i.e.
+    // the live refresh should happen synchronously
+    if (!immediately) {
+      for (var graph in this.graphs) {
+        this.updateGraph(graph);
       }
-    },
-    dataType: 'json',
-    error: function(xhr, textStatus, errorThrown) { console.log(errorThrown); },
-    url: constructUrl(metric),
-    cache: false
-  }).done(function(d) {
-    var source = realMetrics[metric].source;
+    }
+  },
+
+  displayTransform: function(value, period, transform) {
+    if (transform !== undefined && transform !== null && transform.length > 0) {
+      var p = period, x = value;
+      return eval(transform);
+    } else {
+      return value;
+    }
+  },
+
+  // retrieve the data from Graphite
+  getData: function(metric, callback) {
+    $.ajax({
+      beforeSend: function(xhr) {
+        if (auth.length > 0) {
+          var bytes = Crypto.charenc.Binary.stringToBytes(auth);
+          var base64 = Crypto.util.bytesToBase64(bytes);
+          xhr.setRequestHeader("Authorization", "Basic " + base64);
+        }
+      },
+      dataType: 'json',
+      error: function(xhr, textStatus, errorThrown) { console.log(errorThrown); },
+      url: this.constructUrl(metric),
+      cache: false
+    }).done(function(d) {
+      LibratoMetrics.done(metric, callback, d);
+    });
+  },
+
+  done: function(metric, callback, d) {
+    var myDatum = [];
+    var source = this.realMetrics[metric].source;
     var transform = d.attributes.display_transform;
     var period = d.period;
     if (d.measurements[source] !== undefined && d.measurements[source].length > 0) {
       myDatum[0] = {
         x: d.measurements[source][0].measure_time,
-        y: displayTransform(d.measurements[source][0].value, period, transform) || 0
+        y: this.displayTransform(d.measurements[source][0].value, this.period, transform) || 0
       };
       for (var j = 1; j < d.measurements[source].length; j++) {
         myDatum[j] = {
           x: d.measurements[source][j].measure_time,
-          y: displayTransform(d.measurements[source][j].value, period, transform) || graphs[metric].lastKnownValue
+          y: this.displayTransform(d.measurements[source][j].value, this.period, transform) || graphs[metric].lastKnownValue
         };
         if (typeof d.measurements[source][0].value === "number") {
-          graphs[metric].lastKnownValue = d.measurements[source][0].value;
+          this.graphs[metric].lastKnownValue = d.measurements[source][0].value;
         }
       }
-      cb(metric, myDatum);
+      callback.call(LibratoMetrics, metric, myDatum);
     }
-  });
-}
 
-// perform the actual graph object and
-// overlay name and number updates
-function updateGraph(graph) {
-  // update our graph
-  graphs[graph].update();
-  if (datum[graph][datum[graph].length - 1] !== undefined) {
-    var lastValue = datum[graph][datum[graph].length - 1].y;
-    var lastValueDisplay = Math.round(lastValue * 100) / 100;
-    $('.overlay-name' + realMetrics[graph].selector).text(aliases[graph]);
-    $('.overlay-number' + realMetrics[graph].selector).text(lastValueDisplay);
-    if (realMetrics[graph].unit) {
-      $('.overlay-number' + realMetrics[graph].selector).append('<span class="unit">' + realMetrics[graph].unit + '</span>');
-    }
-  } else {
-    $('.overlay-name' + realMetrics[graph].selector).text(aliases[graph]);
-    $('.overlay-number' + realMetrics[graph].selector).html('<span class="error">NF</span>');
-  }
-}
+  },
 
-// add our containers
-function buildContainers() {
-  var falseTargets = 0;
-  for (var metric in metrics) {
-    if (metrics[metric].target === false) {
-      $('#main').append('<div id="false"></div>');
-      falseTargets++;
+  // perform the actual graph object and
+  // overlay name and number updates
+  updateGraph: function(graph) {
+    // update our graph
+    this.graphs[graph].update();
+    if (this.datum[graph][this.datum[graph].length - 1] !== undefined) {
+      var lastValue = this.datum[graph][this.datum[graph].length - 1].y;
+      var lastValueDisplay = Math.round(lastValue * 100) / 100;
+      $('.overlay-name' + this.realMetrics[graph].selector).text(this.aliases[graph]);
+      $('.overlay-number' + this.realMetrics[graph].selector).text(lastValueDisplay);
+      if (this.realMetrics[graph].unit) {
+        $('.overlay-number' + this.realMetrics[graph].selector).append('<span class="unit">' + this.realMetrics[graph].unit + '</span>');
+      }
     } else {
-      $('#main').append(
-        '<div id="graph" class="graph' + metrics[metric].selector + '">' +
-        '<div id="overlay-name" class="overlay-name' + metrics[metric].selector + '"></div>' +
-        '<div id="overlay-number" class="overlay-number' + metrics[metric].selector + '"></div>' +
-        '</div>'
-      );
+      $('.overlay-name' + this.realMetrics[graph].selector).text(this.aliases[graph]);
+      $('.overlay-number' + this.realMetrics[graph].selector).html('<span class="error">NF</span>');
+    }
+  },
+
+  // add our containers
+  buildContainers: function() {
+    var falseTargets = 0;
+    for (var metric in this.metrics) {
+      if (metrics[metric].target === false) {
+        $('#main').append('<div id="false"></div>');
+        falseTargets++;
+      } else {
+        $('#main').append(
+          '<div id="graph" class="graph' + this.metrics[metric].selector + '">' +
+          '<div id="overlay-name" class="overlay-name' + this.metrics[metric].selector + '"></div>' +
+          '<div id="overlay-number" class="overlay-number' + this.metrics[metric].selector + '"></div>' +
+          '</div>'
+        );
+      }
+    }
+  },
+  initialize: function() {
+    this.metrics = metrics;
+    // filter out false targets
+    this.gatherRealMetrics();
+
+    // build our div containers
+    this.buildContainers();
+
+    // build our graph objects
+    this.constructGraphs();
+    // initial load screen
+    for (var graph in this.graphs) {
+      if (this.realMetrics[graph].target === false) {
+        //continue;
+      } else if (myTheme === "dark") {
+        $('.overlay-number' + this.realMetrics[graph].selector).html('<img src="/i/spin-night.gif" />');
+      } else {
+        $('.overlay-number' + this.realMetrics[graph].selector).html('<img src="/i/spin.gif" />');
+      }
     }
   }
 }
 
-// filter out false targets
-gatherRealMetrics();
 
-// build our div containers
-buildContainers();
-
-// build our graph objects
-constructGraphs();
+LibratoMetrics.initialize();
 
 // set our last known value at invocation
 Rickshaw.Graph.prototype.lastKnownValue = 0;
@@ -203,24 +226,14 @@ if (myTheme === "dark") { enableNightMode(); }
 var toolbar = (typeof toolbar == 'undefined') ? true : toolbar;
 if (!toolbar) { $('div#toolbar').css('display', 'none'); }
 
-// initial load screen
-for (var graph in graphs) {
-  if (realMetrics[graph].target === false) {
-    //continue;
-  } else if (myTheme === "dark") {
-    $('.overlay-number' + realMetrics[graph].selector).html('<img src="/i/spin-night.gif" />');
-  } else {
-    $('.overlay-number' + realMetrics[graph].selector).html('<img src="/i/spin.gif" />');
-  }
-}
-refreshData("now");
+LibratoMetrics.refreshData("now");
 
 // define our refresh and start interval
 var refreshInterval = (typeof refresh == 'undefined') ? 20000 : refresh;
-var refreshId = setInterval(refreshData, refreshInterval);
+var refreshId = setInterval(LibratoMetrics.refreshData, refreshInterval);
 
 // set our "live" interval hint
-$('#toolbar ul li.timepanel a.play').text(period + 'min');
+$('#toolbar ul li.timepanel a.play').text(LibratoMetrics.period + 'min');
 
 // activate night mode
 function enableNightMode() {
@@ -257,7 +270,7 @@ $('li.toggle-nonum a').click(function() { $('div#overlay-number').toggleClass('n
 // time panel, pause live feed and show range
 $('#toolbar ul li.timepanel a.range').click(function() {
   period = $(this).attr("title");
-  refreshData("now")
+  LibratoMetrics.refreshData("now")
   if (! $('#toolbar ul li.timepanel a.play').hasClass('pause')) {
     $('#toolbar ul li.timepanel a.play').addClass('pause');
   }
@@ -274,17 +287,17 @@ $('#toolbar ul li.timepanel a.play').click(function() {
   $(this).parent('li').addClass('selected');
   $(this).removeClass('pause');
   $('#toolbar ul li.timepanel a.play').text(period + 'min');
-  refreshData("now");
+  LibratoMetrics.refreshData("now");
   // explicitly clear the old Interval in case
   // someone "doubles up" on the live play button
   clearInterval(refreshId);
   // remove and recreate the original graphs[]
   // helps clear out any rendering artifacts
   $('#graph svg').remove();
-  constructGraphs();
+  LibratoMetrics.constructGraphs();
   // reapply our style settings if night mode is active
   if ($('body').hasClass('night')) { enableNightMode(); }
   // restart our refresh interval
-  refreshId = setInterval(refreshData, refreshInterval);
+  refreshId = setInterval(LibratoMetrics.refreshData, refreshInterval);
 });
 
